2790e0e164c71dd8b0647503fee6ca1af3312192
Andrew Ross <andrew@Andrews-MacBook-Pro.local>
2018-09-19 09:44:16 -0500

    disable twit

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const app = express();
 var Twit = require('twit');
 
 // New Twit with Heroku Variables
-var T = new Twit({
-    consumer_key:         process.env.CONSUMER_KEY,
-    consumer_secret:      process.env.CONSUMER_SECRET,
-    access_token:         process.env.ACCESS_TOKEN,
-    access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
-    // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    // strictSSL:            false,     // optional - requires SSL certificates to be valid.
-})
+// var T = new Twit({
+//     consumer_key:         process.env.CONSUMER_KEY,
+//     consumer_secret:      process.env.CONSUMER_SECRET,
+//     access_token:         process.env.ACCESS_TOKEN,
+//     access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+//     // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+//     // strictSSL:            false,     // optional - requires SSL certificates to be valid.
+// })
 
 
 var port = process.env.PORT || 4000;
