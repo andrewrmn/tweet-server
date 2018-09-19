84d6b3f9b3029c4749ad7e44273d592f92dd2f4b
Andrew Ross <andrew@Andrews-MacBook-Pro.local>
2018-09-19 10:23:08 -0500

    add post

--- server.js
+++ server.js
@@ -34,45 +34,47 @@ app.get('/', (req, res, next) => {
     // })
 
     // If Not Successful
-    if(body.success !== undefined && !body.success){
-        return res.json({"success": false, "msg":"Failed to post tweet"});
-    }
+    // if(body.success !== undefined && !body.success){
+    //     return res.json({"success": false, "msg":"Failed to post tweet"});
+    // }
 
     //If Successful
-    return res.json({"success": true, "msg":"You've hit the server"});
+    //return res.json({"success": true, "msg":"You've hit the server"});
 });
 
 
 
-//
-// app.post('/', (req, res) => {
-//   if(
-//     req.body.captcha === undefined ||
-//     req.body.captcha === '' ||
-//     req.body.captcha === null
-//   ){
-//     return res.json({"success": false, "msg":"Please select captcha"});
-//   }
-//
-//   // Secret Key from Heroku Config Variable 'RECAPTCHA_SECRET'
-//   const secretKey = process.env.RECAPTCHA_SECRET;
-//
-//   // Google's verification URL: https://developers.google.com/recaptcha/docs/verify
-//   const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`;
-//
-//   // Make Request To VerifyURL
-//   request(verifyUrl, (err, response, body) => {
-//     body = JSON.parse(body);
-//
-//     // If Not Successful
-//     if(body.success !== undefined && !body.success){
-//       return res.json({"success": false, "msg":"Failed captcha verification from live server"});
-//     }
-//
-//     //If Successful
-//     return res.json({"success": true, "msg":"Captcha passed from live server"});
-//   });
-// });
+
+app.post('/', (req, res) => {
+  if(
+    req.body.captcha === undefined ||
+    req.body.captcha === '' ||
+    req.body.captcha === null
+  ){
+    return res.json({"success": false, "msg":"Please select captcha"});
+  }
+
+  return res.json({"success": false, "msg":"You've hit the server!"});
+
+  // // Secret Key from Heroku Config Variable 'RECAPTCHA_SECRET'
+  // const secretKey = process.env.RECAPTCHA_SECRET;
+  //
+  // // Google's verification URL: https://developers.google.com/recaptcha/docs/verify
+  // const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`;
+  //
+  // // Make Request To VerifyURL
+  // request(verifyUrl, (err, response, body) => {
+  //   body = JSON.parse(body);
+  //
+  //   // If Not Successful
+  //   if(body.success !== undefined && !body.success){
+  //     return res.json({"success": false, "msg":"Failed captcha verification from live server"});
+  //   }
+  //
+  //   //If Successful
+  //   return res.json({"success": true, "msg":"Captcha passed from live server"});
+  // });
+});
 
 
 app.listen(port, function () {
