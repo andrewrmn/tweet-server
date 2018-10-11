badb5a51e674fe000ef9fb524facbcbdb37517e2
Andrew <andrewross.mn@gmail.com>
2018-10-11 13:09:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -44,29 +44,32 @@ app.post('/', (req, res) => {
 
   var b64content = req.body.media_id;
 
-  return res.json({"success": true});
+  //return res.json({"success": true});
 
     // first we must post the media to Twitter
 
-    // T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-    //     // now we can assign alt text to the media, for use by screen readers and
-    //     // other text-based presentations and interpreters
-    //     var mediaIdStr = data.media_id_string
-    //     var altText = "Small flowers in a planter on a sunny balcony, blossoming."
-    //     var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
-    //
-    //     T.post('media/metadata/create', meta_params, function (err, data, response) {
-    //         if (!err) {
-    //             // now we can reference the media and post a tweet (media will attach to the tweet)
-    //             var params = { status: 'loving life #nofilter', media_ids: [mediaIdStr] }
-    //
-    //             T.post('statuses/update', params, function (err, data, response) {
-    //                 console.log(data);
-    //                 return res.json({"success": data});
-    //             })
-    //          }
-    //     })
-    // })
+    T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+        // now we can assign alt text to the media, for use by screen readers and
+        // other text-based presentations and interpreters
+        var mediaIdStr = data.media_id_string
+        var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+        
+        return res.json({"success": data});
+        
+        
+//         T.post('media/metadata/create', meta_params, function (err, data, response) {
+//             if (!err) {
+//                 // now we can reference the media and post a tweet (media will attach to the tweet)
+//                 var params = { status: 'loving life #nofilter', media_ids: [mediaIdStr] }
+    
+//                 T.post('statuses/update', params, function (err, data, response) {
+//                     console.log(data);
+//                     return res.json({"success": data});
+//                 })
+//              }
+//         })
+    })
 
 
 //   var message = "Hello world, my name is" + req.body.name + "!";
