294105c1a0c0f01580665d3e59fad58b855edf83
Andrew <andrewross.mn@gmail.com>
2018-10-13 09:30:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post('/', (req, res) => {
         return res.json({"success": false, "msg": "name not submitted" });
     }
     
-    return res.json({"success": req.body.image});
+    
 
      AWS.config.update({ accessKeyId: process.env.AWS_KEY, secretAccessKey: process.env.AWS_SECRET });
 
@@ -60,7 +60,7 @@ app.post('/', (req, res) => {
      },function (resp) {
          console.log(arguments);
          console.log('Successfully uploaded package.');
-        // return resp.json({ "success": b64content, "arguments": arguments });
+         return res.json({"success": req.body.image});
      });
 
 
