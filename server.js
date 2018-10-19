30fc3eab16031f3814c124bd12ec732f7176ef77
Andrew <andrewross.mn@gmail.com>
2018-10-19 12:05:44 -0500

    back to twitter

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
-const AWS = require('aws-sdk');
 
+const Twit = require('twit');
+
+// New Twit with Heroku Variables
+var T = new Twit({
+    consumer_key:         process.env.CONSUMER_KEY,
+    consumer_secret:      process.env.CONSUMER_SECRET,
+    access_token:         process.env.ACCESS_TOKEN,
+    access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+    // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    // strictSSL:            false,     // optional - requires SSL certificates to be valid.
+});
 
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 app.use(bodyParser.json({limit: '50mb'}));
 var port = process.env.PORT || 4000;
-
 
 app.use(function(req, res, next) {
     // Important
@@ -17,46 +26,48 @@ app.use(function(req, res, next) {
     next();
 });
 
-
 app.get('/', (req, res, next) => {
-    res.send('Waiting for submissions');
+    res.send('Waiting for tweets');
 });
 
 
+
 app.post('/', (req, res) => {
-    if(
-        req.body.image === undefined ||
-        req.body.image === '' ||
-        req.body.image === null
-    ){
-        return res.json({"success": false, "msg": "img not submitted" });
-    }
+  if(
+    req.body.media_data === undefined ||
+    req.body.media_data === '' ||
+    req.body.media_data === null
+  ){
+    return res.json({"success": false, "msg": "name not submitted" });
+  }
 
-    AWS.config.update({ accessKeyId: process.env.AWS_KEY, secretAccessKey: process.env.AWS_SECRET });
+  var b64content = req.body.media_id;
 
-    let b64content = req.body.image;
-    let decodedImage = Buffer.from(b64content, 'base64');
-    let filename = 'my-octocat-' + Date.now() + '.png';
-    let s3 = new AWS.S3();
+  //return res.json({"success": true});
 
-    s3.putObject({
-        Bucket: 'armntestbucket',
-        Key: filename,
-        Body: decodedImage,
-        ACL: 'public-read'
-    },function (err, resp) {
-        if(err) {
-            return res.json({"success": false });
-       } else {
-            // console.log(arguments);
-            // console.log('Successfully uploaded package.');
-            //var s3url = s3.getSignedUrl('getObject', {Key: filename});
-            imgUrl = 'https://s3.us-east-2.amazonaws.com/armntestbucket/' + filename;
+    first we must post the media to Twitter
+    uncomment to Work
+    T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+        // now we can assign alt text to the media, for use by screen readers and
+        // other text-based presentations and interpreters
+        var mediaIdStr = data.media_id_string
+        var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
 
-            return res.json({"success": true, url: imgUrl });
-       }
-    });
+        T.post('media/metadata/create', meta_params, function (err, data, response) {
+            if (!err) {
+                // now we can reference the media and post a tweet (media will attach to the tweet)
+                var params = { status: 'loving life #nofilter', media_ids: [mediaIdStr] }
 
+                return res.json({"success": params});
+
+                // T.post('statuses/update', params, function (err, data, response) {
+                //     console.log(data);
+                //     return res.json({"success": data});
+                // })
+             }
+        })
+    })
 
 });
 
