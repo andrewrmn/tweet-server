43e5255602bf9212fc6d6357c8cf9de9ac1f8bcb
Andrew <andrewross.mn@gmail.com>
2018-10-20 10:02:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -41,54 +41,85 @@ app.post('/', (req, res) => {
     return res.json({"success": false, "msg": "name not submitted" });
   }
     
-   var _requestSecret;
+  // var _requestSecret;
 
-    //T.get("request-token", function(req, res) {
-        T.getRequestToken(function(err, requestToken, requestSecret) {
-            if (err)
-                res.status(500).send(err);
-             return res.json({"success":'falseo', "msg": "name not submitted" });
-            else {
-                _requestSecret = requestSecret;
-                res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
-            }
-        });
-   // });
+//     T.post('oauth/request_token', function(req, res) {
+//         T.post(function(err, requestToken, requestSecret) {
+//             if (err)
+//                 res.status(500).send(err);
+//              return res.json({"success":'falseo', "msg": "name not submitted" });
+//             else {
+//                 _requestSecret = requestSecret;
+//                 res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
+//             }
+//         });
+//    // });
 
-  var b64content = req.body.media_data;
+    //var b64content = req.body.media_data;
+    
+    
+    var imgUrl = 'https://octocat-generator-assets.githubusercontent.com/my-octocat-1540047452431.png';    
+    var emailAddress = 'andrewross.mn@gmail.com';
+    var source = 'web';
+    var formName = 'octocat-generator-form';
+    var formId = '88570519';
+    var http = new XMLHttpRequest();
+    var url = 'https://s88570519.t.eloqua.com/e/f2';
+    var params = 'elqFormName='+ formName +'&elqSiteID='+ formId +'&emailAddress='+ emailAddress +'&gitHubOctocatURL='+ imgUrl +'&sourceURL='+ source +'';
+    
+    
+    app.post(url, params, (req, res) => {
+        return res.json({'success': 'truedat', 'url': imgUrl });
+    });
+    
+    http.open('POST', url, true);
+    
+    //Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            console.log('email sent');
+        }
+    };
+    http.send(params);
+
+    return res.json({'success': 'truedat', 'url': imgUrl });
+   
+
 
   //return res.json({"success": true});
 
     //first we must post the media to Twitter
     //uncomment to Work
-    T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-        // now we can assign alt text to the media, for use by screen readers and
-        // other text-based presentations and interpreters
-        var mediaIdStr = data.media_id_string
-        var altText = "Small flowers in a planter on a sunny balcony, blossoming."
-        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+//     T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+//         // now we can assign alt text to the media, for use by screen readers and
+//         // other text-based presentations and interpreters
+//         var mediaIdStr = data.media_id_string
+//         var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+//         var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
 
-        T.post('media/metadata/create', meta_params, function (err, data, response) {
-            if (!err) {
-                // now we can reference the media and post a tweet (media will attach to the tweet)
-                var params = { status: 'loving life #nofilter', media_ids: [mediaIdStr] }
+//         T.post('media/metadata/create', meta_params, function (err, data, response) {
+//             if (!err) {
+//                 // now we can reference the media and post a tweet (media will attach to the tweet)
+//                 var params = { status: 'loving life #nofilter', media_ids: [mediaIdStr] }
                    
-//                 var getParams = { command: 'STATUS', media_id: [mediaIdStr] }
+// //                 var getParams = { command: 'STATUS', media_id: [mediaIdStr] }
                 
-//                 T.get('statuses/update', getParams, function (err, data, response) {
-//                     console.log(data);
-//                     return res.json({"success": data});
-//                 })
+// //                 T.get('statuses/update', getParams, function (err, data, response) {
+// //                     console.log(data);
+// //                     return res.json({"success": data});
+// //                 })
                 
-                return res.json({"success": true, "mediaId": data});
+//                 return res.json({"success": true, "mediaId": data});
                 
-                // T.post('statuses/update', params, function (err, data, response) {
-                //     console.log(data);
-                //     return res.json({"success": data});
-                // })
-             }
-        })
-    })
+//                 // T.post('statuses/update', params, function (err, data, response) {
+//                 //     console.log(data);
+//                 //     return res.json({"success": data});
+//                 // })
+//              }
+//         })
+//     })
 
 });
 
