f9130cb2f320e43533d0e48cd7bd6ebc9dfd0137
Andrew <andrewross.mn@gmail.com>
2018-10-20 10:06:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', (req, res, next) => {
-    res.send('Waiting for tweets');
+    res.send('Waiting for tweets...');
 });
 
 
@@ -68,23 +68,21 @@ app.post('/', (req, res) => {
     var params = 'elqFormName='+ formName +'&elqSiteID='+ formId +'&emailAddress='+ emailAddress +'&gitHubOctocatURL='+ imgUrl +'&sourceURL='+ source +'';
     
     
-    app.post(url, params, (req, res) => {
-        return res.json({'success': true', 'url': imgUrl });
-    });
-    
-//     http.open('POST', url, true);
-    
-//     //Send the proper header information along with the request
-//     http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-//     http.onreadystatechange = function() {//Call a function when the state changes.
-//         if(http.readyState == 4 && http.status == 200) {
-//             console.log('email sent');
-//         }
-//     };
-//     http.send(params);
+    
+    http.open('POST', url, true);
+    
+    //Send the proper header information along with the request
+    //http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-//     return res.json({'success': true, 'url': imgUrl });
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            console.log('email sent');
+        }
+    };
+    http.send(params);
+
+    return res.json({'success': true, 'url': imgUrl });
    
 
 
