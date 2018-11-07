48610323b4b9342641aadb259177964a66bed5bf
Andrew <andrewross.mn@gmail.com>
2018-11-07 16:33:06 -0600

    Update server.js

--- server.js
+++ server.js
@@ -45,13 +45,14 @@ app.post('/', (req, res) => {
   
   var callbackUrl = encodeURI('https://twitter.com/ireckonimdrew/gls-app');
     
-  T.post('oauth/request_token', { oauth_callback : callbackUrl } function(err, requestToken, requestSecret) {
+  T.post('oauth/request_token', { oauth_callback : callbackUrl } function(err, data, response) {
       if (err)
-          res.status(500).send(err);
+          //res.status(500).send(err);
           return res.json({"success":'oauth error' });
       else {
-          _requestSecret = requestSecret;
-          res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
+          return res.json({"success": data });
+         /// _requestSecret = requestSecret;
+         // res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
       }
   });
    
