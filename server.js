bf8ed8cbea582338e0e5c174f54c03628376344b
Andrew <andrewross.mn@gmail.com>
2018-11-08 10:48:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -48,10 +48,10 @@ app.post('/', (req, res) => {
     
   var callbackUrl = encodeURIComponent(cUrl);
     
-  T.post('https://api.twitter.com/oauth/request_token', { 'oauth_callback' : 'oob' }, function(err, data, response) {
+  T.post('https://api.twitter.com/oauth/request_token', function(err, data, response) {
       if (err)
           //res.status(500).send(err);
-          return res.json({"success": data });
+          return res.json({"success": response });
       else {
           return res.json({"success": data });
          /// _requestSecret = requestSecret;
