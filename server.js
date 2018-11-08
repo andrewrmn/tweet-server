442805777511db71c80f2fddf39d3313e5af7c65
Andrew <andrewross.mn@gmail.com>
2018-11-08 10:10:17 -0600

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ var Twit = require('twit');
 var T = new Twit({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
-    app_only_auth: true,
+    //app_only_auth: true,
     //access_token:         process.env.ACCESS_TOKEN,
     //access_token_secret:  process.env.ACCESS_TOKEN_SECRET
     // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL:            false     // optional - requires SSL certificates to be valid.
+    //strictSSL:            false     // optional - requires SSL certificates to be valid.
 });
 
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
