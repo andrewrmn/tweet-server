ba6bae86610b1101fbda9cc6683f0eece9ce38f8
Andrew <andrewross.mn@gmail.com>
2018-11-08 09:18:29 -0600

    Update server.js

--- server.js
+++ server.js
@@ -50,20 +50,20 @@ app.post('/', (req, res) => {
   var callbackUrl = encodeURIComponent(cUrl);
     
     
-  app.post('https://api.twitter.com/oauth/request_token', { oauth_callback : cUrl }, (req, res) => {
-        return res.json({"success":req });
-  });
-    
-//   T.post('oauth/request_token', { oauth_callback : cUrl }, function(err, data, response) {
-//       if (err)
-//           //res.status(500).send(err);
-//           return res.json({"success":data });
-//       else {
-//           return res.json({"success": data });
-//          /// _requestSecret = requestSecret;
-//          // res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
-//       }
+//   app.post('https://api.twitter.com/oauth/request_token', { oauth_callback : cUrl }, (req, res) => {
+//         return res.json({"success":req });
 //   });
+    
+  T.getAuth({ oauth_callback : cUrl }, function(err, data, response) {
+      if (err)
+          //res.status(500).send(err);
+          return res.json({"success":data });
+      else {
+          return res.json({"success": data });
+         /// _requestSecret = requestSecret;
+         // res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
+      }
+  });
    
     
 //     T.get('account/verify_credentials', { skip_status: true })
