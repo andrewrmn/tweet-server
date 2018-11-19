1ec143f9e97a4a1d5c9cf0a5f95f61eabc53b98c
Andrew <andrewross.mn@gmail.com>
2018-11-19 10:15:06 -0600

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ var T = new Twit({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     //app_only_auth: false,
-    access_token: '',
-    access_token_secret: ''
-    //access_token: process.env.ACCESS_TOKEN,
-    //access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
     // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
     //strictSSL:            false     // optional - requires SSL certificates to be valid.
 });
