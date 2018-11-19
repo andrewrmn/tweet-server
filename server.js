75e434e0cdd3d2845fd2ae0fa38b96f0cd95408d
Andrew <andrewross.mn@gmail.com>
2018-11-19 10:48:45 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
+var Twitter = require('twitter');
 
 var OAuth= require('oauth').OAuth;
 
