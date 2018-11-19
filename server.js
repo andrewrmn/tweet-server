74b0b0d6fb8f52ca36596b4fc270219126c37952
Andrew Ross <andrew@Andrews-MacBook-Pro.local>
2018-11-19 11:42:43 -0600

    Checkin in modules

--- server.js
+++ server.js
@@ -6,18 +6,23 @@ var Twitter = require('twitter');
 
 var OAuth= require('oauth').OAuth;
 
-// var Twit = require('twit');
 
-// // // New Twit with Heroku Variables
-// var T = new Twit({
-//     consumer_key: process.env.CONSUMER_KEY,
-//     consumer_secret: process.env.CONSUMER_SECRET,
-//     //app_only_auth: false,
-//     access_token: process.env.ACCESS_TOKEN,
-//     access_token_secret: process.env.ACCESS_TOKEN_SECRET
-//     // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-//     //strictSSL:            false     // optional - requires SSL certificates to be valid.
-// });
+
+var TWITTER_CONSUMER_KEY = 'K5FIfgCiyqrzRG2u5Y2vKTIMv';
+var TWITTER_CONSUMER_SECRET = '6NJ0VW3DzG3hxWSzmRFPwfgAc9AF5X4ftc4wyDQluhf4a1WzCk';
+var oat="";
+var oas="";
+var oav="";
+
+var oa = new OAuth(
+    "https://api.twitter.com/oauth/request_token",
+    "https://api.twitter.com/oauth/access_token",
+    TWITTER_CONSUMER_KEY ,
+    TWITTER_CONSUMER_SECRET,
+    "1.0",
+    "https://twitter.com/ireckonimdrew/gls-app",
+    "HMAC-SHA1"
+);
 
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 app.use(bodyParser.json({limit: '50mb'}));
@@ -31,161 +36,47 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', (req, res, next) => {
-    res.send('Waiting for tweets...');
+    res.send('Waiting for tweets');
 });
 
 
 app.post('/', (req, res) => {
-  if(
-    req.body.media_id === undefined ||
-    req.body.media_id === '' ||
-    req.body.media_id === null
-  ){
-    return res.json({"success": false, "msg": "name not submitted" });
-  }
+    if(
+        req.body.media_id === undefined ||
+        req.body.media_id === '' ||
+        req.body.media_id === null
+    ){
+        return res.json({"success": false, "msg": "name not submitted" });
+    }
 
-  var b64content = req.body.media_id;
+    var b64content = req.body.media_id;
 
-  var cUrl = 'https://twitter.com/ireckonimdrew/gls-app';
-  var cUrl = 'https://andrewross.co/';
+    var cUrl = 'https://twitter.com/ireckonimdrew/gls-app';
+    var cUrl = 'https://andrewross.co/';
 
-  var callbackUrl = encodeURIComponent(cUrl);
+    var callbackUrl = encodeURIComponent(cUrl);
 
-//   T.post('https://api.twitter.com/oauth/request_token', {'oauth_callback': 'https://andrewross.co/', 'oauth_token_secret': ''}, function(err, data, response) {
-//       if (err)
-//           //res.status(500).send(err);
-//           return res.json({"success": data });
-//       else {
-//           return res.json({"success": data });
-//          /// _requestSecret = requestSecret;
-//          // res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
-//       }
-//   });
-    
-    //app.post('https://api.twitter.com/oauth/request_token', function (req, res) {
-        var oat="";
-        var oas="";
-        var oav="";
-        var cb = 'https://andrewross.co/';
-        
-      
-        var oa = new OAuth(
-            "https://api.twitter.com/oauth/request_token",	
-            "https://api.twitter.com/oauth/access_token",
-            process.env.CONSUMER_KEY,
-            process.env.CONSUMER_SECRET,
-            "1.0",
-            cb,
-            "HMAC-SHA1"
-        );
-        
-        oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
-            if (error) {
-                console.log(error);
-                return res.json({"success": error });
+    oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+        if (error) {
+            console.log(error);
+            return res.json({"success": error });
+        }
+        else {
+            //req.session.oauth = {};
+            //req.session.oauth.token = oauth_token;
+            console.log('oauth.token: ' + oauth_token);
+            //req.session.oauth.token_secret = oauth_token_secret;
+            console.log('oauth.token_secret: ' + oauth_token_secret);
+            oat=oauth_token;
+            oas=oauth_token_secret;
+            var response={
+                oauth_token: oauth_token,
+                oauth_token_secret: oauth_token_secret
             }
-            else {
-                //req.session.oauth = {};
-                //req.session.oauth.token = oauth_token;
-                console.log('oauth.token: ' + oauth_token);
-                //req.session.oauth.token_secret = oauth_token_secret;
-                console.log('oauth.token_secret: ' + oauth_token_secret);
-                oat=oauth_token;
-                oas=oauth_token_secret;
-                var response={
-                    oauth_token: oauth_token,
-                    oauth_token_secret: oauth_token_secret
-                }
-                 return res.send(response)
-                //res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
-            }
-        });
-
-
-
-
-    
-
-//     var cKey = T.getAuth().consumer_key;
-//     var cSec = T.getAuth().consumer_secret;
-
-//     return res.json({"success": T.getAuth().consumer_key });
-
-
-//     T.get('account/verify_credentials', { skip_status: true })
-//       .catch(function (err) {
-//         console.log('caught error', err.stack)
-//       })
-//       .then(function (result) {
-//         // `result` is an Object with keys "data" and "resp".
-//         // `data` and `resp` are the same objects as the ones passed
-//         // to the callback.
-//         // See https://github.com/ttezel/twit#tgetpath-params-callback
-//         // for details.
-
-//         console.log('data', result.data);
-//       })
-
-  //T.post('oauth/request_token', function(req, res) {
-//       T.post('oauth/request_token', function(err, requestToken, requestSecret) {
-//           if (err)
-//               res.status(500).send(err);
-//            return res.json({"success":'falseo', "msg": "name not submitted" });
-//           else {
-//               _requestSecret = requestSecret;
-//               res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
-//           }
-//       });
-  //});
-
-
-
-  //return res.json({'success': true });
-
-    // first we must post the media to Twitter
-//     T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-//         // now we can assign alt text to the media, for use by screen readers and
-//         // other text-based presentations and interpreters
-//         var mediaIdStr = data.media_id_string;
-//         var mediaId = data.media_id;
-//         var mediaData = data;
-//         var altText = "Small flowers in a planter on a sunny balcony, blossoming."
-//         var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
-
-//         T.post('media/metadata/create', meta_params, function (err, data, response) {
-//             if (!err) {
-//                 // now we can reference the media and post a tweet (media will attach to the tweet)
-//                 var params = { status: '#myOctocat', media_ids: [mediaIdStr] }
-
-// //                 T.get('media/upload', { command: 'STATUS', media_id: mediaId }, function (err, data, response) {
-// //                      if (!err) {
-// //                           return res.json({'success': data });
-// //                      } else {
-// //                          return res.json({'success': false });
-// //                      }
-// //                 });
-
-//                 //return res.json({'success': false });
-
-//                 //return res.json({"success": params});
-
-//                 T.post('statuses/update', params, function (err, data, response) {
-//                     //console.log(data);
-//                     return res.json({"success": data.entities.media[0].media_url});
-//                 })
-//              } else {
-//                 return res.json({'success': 'falseO' });
-//              }
-//         })
-//     })
-
-
-  // var message = "Hello world, my name is" + req.body.name + "!";
-  // T.post('statuses/update', { status: message }, function(err, data, response) {
-  //     //console.log(data);
-  //     return res.json({"success": data});
-  // })
-
+            //return res.send(response)
+            res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
+        }
+    });
 
 });
 
